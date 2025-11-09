5926a4893b3aafc47b18db72beca1bda271c298a
pawanemb <pawan.patra@emb.global>
2025-11-09 21:59:04 +0530

    dlete

--- src/layout/AppSidebar.tsx
+++ src/layout/AppSidebar.tsx
@@ -45,33 +45,9 @@ const navItems: NavItem[] = [
     path: "/projects",
   },
   {
-    icon: <CalenderIcon />,
-    name: "Calendar",
-    path: "/calendar",
-  },
-  {
     icon: <UserCircleIcon />,
     name: "User Profile",
     path: "/profile",
-  },
-
-  {
-    name: "Forms",
-    icon: <ListIcon />,
-    subItems: [{ name: "Form Elements", path: "/form-elements", pro: false }],
-  },
-  {
-    name: "Tables",
-    icon: <TableIcon />,
-    subItems: [{ name: "Basic Tables", path: "/basic-tables", pro: false }],
-  },
-  {
-    name: "Pages",
-    icon: <PageIcon />,
-    subItems: [
-      { name: "Blank Page", path: "/blank", pro: false },
-      { name: "404 Error", path: "/error-404", pro: false },
-    ],
   },
 ];
 
