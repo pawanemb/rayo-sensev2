bb9b6b683bc5a7fabe5d5bf5ec629be75abc9aff
pawanemb <pawan.patra@emb.global>
2025-11-12 22:50:03 +0530

    Update MonthlySalesChart.tsx

--- src/components/ecommerce/MonthlySalesChart.tsx
+++ src/components/ecommerce/MonthlySalesChart.tsx
@@ -262,7 +262,7 @@ export default function MonthlySalesChart() {
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1} d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
             </svg>
             <p className="text-lg font-medium mb-1">No data available</p>
-            <p className="text-sm opacity-75">No customers found in the last 6 months</p>
+            <p className="text-sm opacity-75">No users found in the last 6 months</p>
           </div>
         ) : (
           <div className="-ml-5 min-w-[650px] xl:min-w-full pl-2">
